0943e53f6417a621ad061017d83ca37bd184cdd2
Guilherme Abreu <sabreuguilherme@gmail.com>
2021-06-03 11:05:00 -0300

    Added solution

---
+++ br/com/abreu/leetcode/challenge/june/DayOne.java
@@ -0,0 +1,32 @@
+package br.com.abreu.leetcode.challenge.june;
+
+/**
+ * Max Area of Island
+ */
+public class DayOne {
+
+    public static void main(String[] args) {
+        int[][] grid = {
+                {0,0,1,0,0,0,0,1,0,0,0,0,0},
+                {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                {0,1,1,0,1,0,0,0,0,0,0,0,0},
+                {0,1,0,0,1,1,0,0,1,0,1,0,0},
+                {0,1,0,0,1,1,0,0,1,1,1,0,0},
+                {0,0,0,0,0,0,0,0,0,0,1,0,0},
+                {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                {0,0,0,0,0,0,0,1,1,0,0,0,0}
+        };
+
+        System.out.println(maxAreaOfIsland(grid));
+    }
+
+    public static int maxAreaOfIsland(int[][] grid){
+        for (int i=0; i < grid.length; i++){
+            System.out.println(i);
+            for(int j = 0; j < grid[i].length; j++){
+            }
+
+        }
+        return 0;
+    }
+}
