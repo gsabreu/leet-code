0b3685ec17e5f2b961d33c745a531189471d44d1
Guilherme Abreu <sabreuguilherme@gmail.com>
2021-06-04 16:43:04 -0300

    Alien dictionary

---
+++ br/com/abreu/leetcode/problems/easy/AlienDictionary.java
@@ -0,0 +1,26 @@
+package br.com.abreu.leetcode.problems.easy;
+
+public class AlienDictionary {
+
+    public static boolean isAlienSorted(String[] words, String order) {
+        for(int i=0 ; i<words.length-1 ; i++){
+            boolean same = true;
+            String a = words[i], b = words[i+1];
+            for(int k=0 ; k<Math.min(a.length(),b.length()) ; k++){
+                if(a.charAt(k) != b.charAt(k)){
+                    same = false;
+                    int index_a = order.indexOf(a.charAt(k));
+                    int index_b = order.indexOf(b.charAt(k));
+                    if(index_b < index_a){
+                        return false;
+                    }
+                    break;
+                }
+            }
+            if(same && a.length() > b.length()){
+                return false;
+            }
+        }
+        return true;
+    }
+}
